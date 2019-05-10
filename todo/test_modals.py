d53462c432bf067fa75a6d21475143e12b86b56c
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-05-10 12:59:44 +0000

    add new test cases to get 100% test coverage

--- todo/test_modals.py
+++ todo/test_modals.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Item
 
 # Create your tests here.
+class TestItemModal(TestCase):
+    def test_done_defaults_to_False(self):
+        item = Item(name="Test done")
+        item.save()
+        self.assertEqual(item.name, "Test done")
+        self.assertFalse(item.done)
+        
+    def test_can_create_an_item_with_a_name_status(self):
+        item = Item(name="Test done", done=True)
+        item.save()
+        self.assertEqual(item.name, "Test done")
+        self.assertTrue(item.done)
+        
+    def test_item_a_string(self):
+        item = Item(name="Test Item")
+        self.assertEqual("Test Item", str(item))
